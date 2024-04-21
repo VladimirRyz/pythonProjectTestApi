c0c0002a1b2cea8ba02d171611053884f24d76f0
Vladimir <mirav3@yandex.ru>
2024-04-21 13:36:01 +0300

    new test 10

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -24,42 +24,48 @@ def negative_assert(name):
     res = sender_stand_request.post_new_client_kit(authToken, name)
     assert res.status_code == 400
 # Тесты по номерам в соответствии с чек-листом
-def test_1():
+
+def test_one_leter_in_name_get_success_response():
      positive_assert('a')
 
-def test_2():
+def test_511_leter_in_name_get_success_response():
     positive_assert('AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC')
 
-def test_3():
+def test_null_leter_in_name_get_success_response():
     negative_assert('')
 
 
-def test_4():
+def test_512_leter_in_name_get_success_response():
     negative_assert('AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD')
 
 
-def test_5():
+def test_english_leter_in_name_get_success_response():
     positive_assert('QWErty')
 
-def test_6():
+def test_russian_leter_in_name_get_success_response():
     positive_assert('Мария')
 
 
-def test_7():
+def test_special_characters_leter_in_name_get_success_response():
     positive_assert('"№%@",')
 
 
-def test_8():
+def test_spaces_allowed_leter_in_name_get_success_response():
     positive_assert('Человек и КО')
 
 
-def test_9():
+def test_numbers_allowed_leter_in_name_get_success_response():
     positive_assert('123')
 
+#Функция негативной проверки на отсутвие обязательного параметра
+def negative_assert_no_first_name(name):
+    authToken = get_token()
+    res = sender_stand_request.post_new_client_kit(authToken,name)
+    assert res.status_code == 400
+    assert res.json()["message"] == "Не все необходимые параметры были переданы"
+def test_parameter_not_passed_in_request():
+    negative_assert_no_first_name(None)
 
-def test_10():
-    negative_assert()
 
-
-def test_11():
+def test_a_different_parameter_type_number_was_passed():
     negative_assert(123)
